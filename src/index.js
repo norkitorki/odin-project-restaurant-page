7cddf32c8bac7fe3436924e04a80d204a9c1fe8d
norkitorki <norki@tuta.io>
2023-11-13 08:07:42 +0100

    Define pullTemplateContent()

--- src/index.js
+++ src/index.js
@@ -8,3 +8,15 @@ function appendElements(...elements) {
   elements.forEach(el => this.appendChild(el));
 };
 
+const pullTemplateContent = (e) => {
+  if (e.target.nodeName !== 'A') return;
+
+  const targetId = e.target.textContent.toLowerCase();
+  const template = document.getElementById(targetId).content.cloneNode(true);
+  main.innerHTML = '';
+  main.appendChild(template);
+  if (targetId === 'gallery') {
+    initGallery(gallery);
+  }
+};
+
