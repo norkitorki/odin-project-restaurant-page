8c901ea72f78f8b746ca3218c9800e065e2a33bd
norkitorki <norki@tuta.io>
2023-11-14 07:32:10 +0100

    Configure webpack for images

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,13 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        test: /\.(woff|woff2|eot|ttf|otf|)$/i,
         type: 'asset/resource',
       },
-    ],
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
   },
 };
