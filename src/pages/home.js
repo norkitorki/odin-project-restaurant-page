7fd28c53dd45a0322f4ab2e2999dff7252e5ef66
norkitorki <norki@tuta.io>
2023-11-16 13:42:33 +0100

    Move page templates into pages/ and add home.js logic

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+function homeTemplate() {
+  const heading     = document.createElement('h1'),
+        description = document.createElement('p');
+
+  heading.classList.add('main-heading');
+  heading.textContent = 'Odin Restaurant';
+
+  description.classList.add('description');
+  description.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+
+  this.appendChild(heading);
+  this.appendChild(description);
+};
+
+export { homeTemplate };
